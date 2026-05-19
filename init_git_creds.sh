387d5d7761f7a48b93d7178ebe78737137e09dd8
christopherpandalee <christopherpandalee@gmail.com>
2026-05-19 14:30:38 +0000

    Added two init files

---
+++ init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=christopherpandalee@gmail.com
+NAME=christopherpandalee
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name ${NAME}
+
+git config --global --list
