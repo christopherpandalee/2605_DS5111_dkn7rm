d2da48976c79716ccca374ce13c099ad9ae93ed6
Ubuntu <ubuntu@ip-172-31-94-83.ec2.internal>
2026-07-30 17:44:33 +0000

    Trying to get records into snowflake

--- transform/01_stg_youtube_transcripts.sql
+++ transform/01_stg_youtube_transcripts.sql
@@ -4,7 +4,7 @@
 CREATE OR REPLACE TABLE STG_YOUTUBE_TRANSCRIPTS AS
 SELECT
     data:video_id::STRING AS VIDEO_ID,
-    data:cleaned_text::STRING AS CLEANED_TEXT,
+    data:raw_text::STRING AS CLEANED_TEXT,
     data:tech_terms AS TECH_TERMS_ARRAY,
     data:book_names AS BOOK_NAMES_ARRAY,
     loaded_at
