1fe4f1f3e0087d7a5385461432d36d21c5cc4103
Ubuntu <ubuntu@ip-172-31-94-83.ec2.internal>
2026-07-30 18:28:40 +0000

    Edited Lab 10 files

--- models/mart_tech_term_pivot.sql
+++ models/mart_tech_term_pivot.sql
@@ -8,6 +8,6 @@ SELECT
     SUM(CASE WHEN LOWER(tech_term) = '{{ term }}' THEN 1 ELSE 0 END) AS count_{{ term }}_mentions
     {% if not loop.last %},{% endif %}
     {% endfor %}
--- FROM {{ ref('fct_entities_terms') }}  NB:!!!! This notation tells DBT it's a defined model
-FROM {{ source('my_database_sources', 'FCT_ENTITIES_TERMS') }}  -- This notation says it pre-exists
+-- FROM {{ ref('fct_tech_terms') }}  NB:!!!! This notation tells DBT it's a defined model
+FROM {{ source('my_database_sources', 'FCT_TECH_TERMS') }}  -- This notation says it pre-exists
 GROUP BY video_id
