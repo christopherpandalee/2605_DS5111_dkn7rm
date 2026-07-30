b16461651e0fa51876b5219e4901f27b87cc5d06
Ubuntu <ubuntu@ip-172-31-94-83.ec2.internal>
2026-07-30 15:43:40 +0000

    Edited transform files

--- transform/orchestrate_pipeline.sql
+++ transform/orchestrate_pipeline.sql
@@ -1,8 +1,8 @@
 -- Master Pipeline Orchestrator
 -- EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/transform/01_stg_youtube_transcripts.sql;
 -- EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/transform/02_dim_videos.sql;
---EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/transform/03_fct_entities.sql;
+-- EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/transform/03_fct_entities.sql;
 
-EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB10_dbt_expectations/transform/01_stg_youtube_transcripts.sql
-EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB10_dbt_expectations/transform/02_dim_videos.sql
-EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB10_dbt_expectations/transform/03_fct_entities.sql
+EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB10_dbt_expectations/transform/01_stg_youtube_transcripts.sql;
+EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB10_dbt_expectations/transform/02_dim_videos.sql;
+EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB10_dbt_expectations/transform/03_fct_entities.sql;
