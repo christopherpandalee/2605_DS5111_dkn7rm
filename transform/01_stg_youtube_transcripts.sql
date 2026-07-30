f96c6a913772b28e2ad9a703d2425f9b76988c33
Ubuntu <ubuntu@ip-172-31-94-83.ec2.internal>
2026-07-30 20:45:41 +0000

    Edit transform 01

--- transform/01_stg_youtube_transcripts.sql
+++ transform/01_stg_youtube_transcripts.sql
@@ -4,7 +4,7 @@
 CREATE OR REPLACE TABLE STG_YOUTUBE_TRANSCRIPTS AS
 SELECT
     data:video_id::STRING AS VIDEO_ID,
-    data:raw_text::STRING AS CLEANED_TEXT,
+    data:cleaned_text::STRING AS CLEANED_TEXT,
     data:tech_terms AS TECH_TERMS_ARRAY,
     data:book_names AS BOOK_NAMES_ARRAY,
     loaded_at
